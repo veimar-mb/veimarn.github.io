4afe0a50c8ba0594935f5762f7d520efc26ad66a
veimar-n <nveimar@gmail.com>
2019-04-21 23:42:34 -0500

    Add assets, improve css and html code, add javascript file

---
+++ script.js
@@ -0,0 +1,19 @@
+let mainNavLinks = document.querySelectorAll("nav ul li a");
+let mainSections = document.querySelectorAll("container section");
+
+let lastId;
+let cur = [];
+
+window.addEventListener("scroll", event => {
+	let fromTop = window.scrollY;
+
+	mainNavLinks.forEach(link => {
+		let section = document.querySelector(link.hash);
+
+		if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
+			link.classList.add("active");
+		} else {
+			link.classList.remove("active");
+		}
+	});
+});
